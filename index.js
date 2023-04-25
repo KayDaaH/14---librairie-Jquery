a5fec5c3190d7d1dba25c262fe5c1705cd6517b8
KayDaaH <thomas36.rolland@gmail.com>
2023-04-25 04:42:24 +0200

    Reprise du projet

--- index.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const app = express();
-
-app.get("/", (req, res) => {
-  console.log("Salut à tous !");
-});
-
-app.listen(3005);
-console.log("Attente de requête au port 3005");
