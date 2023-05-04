d6a230d2486f55f815a7fc5108d42ad3afca0d81
KayDaaH <thomas36.rolland@gmail.com>
2023-05-04 14:31:55 +0200

    Ajustement dimensions et style

--- frontend/src/pages/EmployeeList/EmployeeList.jsx
+++ frontend/src/pages/EmployeeList/EmployeeList.jsx
@@ -20,50 +20,6 @@ function EmployeeList() {
     <div className={styles.main}>
       <h1>Current Employees</h1>
       <Table data={data} />
-      {/* <div className={styles.searchBar}> */}
-      {/* <div className={styles.selectEntries}>
-          <label htmlFor="search">Show </label>
-          <select name="search" id="search">
-            <option value="100">100</option>
-            <option value="10">10</option>
-            <option value="25">25</option>
-            <option value="50">50</option>
-          </select>
-          <p>entries</p>
-        </div>
-        <form>
-          <label htmlFor="search">Search : </label>
-          <input type="text" id="search" />
-        </form>
-      </div>
-      <div className={styles.listContainer}>
-        <div className={styles.listSort}>
-          <p>First Name</p>
-          <p>Last Name</p>
-          <p>Start Date</p>
-          <p>Department</p>
-          <p>Date of Birth</p>
-          <p>Street</p>
-          <p>City</p>
-          <p>State</p>
-          <p>Zip Code</p>
-        </div>
-        <ul className={styles.employeeList}>
-          {data.map((employee) => (
-            <li key={employee._id}>
-              <p>{employee.firstName}</p>
-              <p>{employee.lastName}</p>
-              <p>{employee.startDate}</p>
-              <p>{employee.department}</p>
-              <p>{employee.dateOfBirth}</p>
-              <p>{employee.street}</p>
-              <p>{employee.city}</p>
-              <p>{employee.State}</p>
-              <p>{employee.zipCode}</p>
-            </li>
-          ))}
-        </ul>
-      </div> */}
       <Link to={"/"}>
         <button className={styles.btnNavigation}>Home</button>
       </Link>
